169a8bd446dbafbbcc1349e96056ae6e24c03bf0
Scott0275 <146986956+Scott0275@users.noreply.github.com>
2025-02-14 08:32:15 +0100

    new

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -4,7 +4,7 @@ function WelcomeMessage() {
   return (
     <div>
       <h1>Hello everyone, I am learning React at ALX!</h1>
-      <p>This is a simple JSX component.</p>
+      <p>I am learning about JSX! </p>
       <p>Hello everyone, I am learning React at ALX.</p> 
       <p>I am learning about JSX</p> 
     </div>
