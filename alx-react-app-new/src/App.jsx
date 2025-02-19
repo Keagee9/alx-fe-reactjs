c0984c32055607d54507d1d124253464e6ce73ed
Scott0275 <146986956+Scott0275@users.noreply.github.com>
2025-02-19 17:05:28 +0100

    week10

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-
+import Counter from `./components/Counter.jsx`
 
 
 function App() {
