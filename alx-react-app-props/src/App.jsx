de83e83807d706db6bca05f21b86901603f4f97d
Scott0275 <146986956+Scott0275@users.noreply.github.com>
2025-02-19 23:24:42 +0100

    mee

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -129,3 +129,30 @@ function App() {
 }
 
 export default App;
+
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+import Navbar from './Navbar'; // Import the Navbar component
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar /> {/* Include the Navbar */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
