38b0530d17a2b82bba7d59cfd49d1e0d79f17fc4
Scott0275 <146986956+Scott0275@users.noreply.github.com>
2025-02-28 11:49:04 +0100

    recipe

--- recipe-sharing-app/src/components/AddRecipeForm.jsx
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -1,21 +1,3 @@
-// RecipeList component
-import { useRecipeStore } from './recipeStore';
-
-const RecipeList = () => {
-  const recipes = useRecipeStore(state => state.recipes);
-
-  return (
-    <div>
-      {recipes.map(recipe => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
-
 // AddRecipeForm component
 import { useState } from 'react';
 import { useRecipeStore } from './recipeStore';
