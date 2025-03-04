781d8cb1995cead2e8bbe2c7ddc8c79fad7a48e4
Scott0275 <146986956+Scott0275@users.noreply.github.com>
2025-03-04 15:14:06 +0100

    recipe

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,48 +1,6 @@
-import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import PostsComponent from './components/PostsComponent'; // We'll create this component next
-
-const queryClient = new QueryClient();
-
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <PostsComponent />
-    </QueryClientProvider>
-  );
-}
 
 
-
-
-
-import React from 'react';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'; // Import routing components
-import PostsComponent from './components/PostsComponent'; // Import your PostsComponent
-import AnotherComponent from './components/AnotherComponent'; // Import another component (create this if needed)
-
-function App() {
-  return (
-    <BrowserRouter>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Posts</Link>
-            </li>
-            <li>
-              <Link to="/another">Another Page</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<PostsComponent />} />
-          <Route path="/another" element={<AnotherComponent />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
-}
-
-export default App;
+<Route element={<ProtectedRoute />}>
+  <Route path="/profile/*" element={<Profile />} />
+  <Route path="/blog/:postId" element={<BlogPost />} />
+</Route>
--- react-router-advanced/src/main.jsx
+++ react-router-advanced/src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from 'react-query'; // Import QueryClient and QueryClientProvider
-import PostsComponent from './components/PostsComponent'; // Import your PostsComponent
-
-const queryClient = new QueryClient(); // Create a new QueryClient instance
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App'; // Import your main App component
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}> {/* Provide the QueryClient to your app */}
-      <PostsComponent />
-    </QueryClientProvider>
+    <BrowserRouter> {/* Wrap your application with BrowserRouter */}
+      <Routes> {/* Use Routes to define your routes */}
+        <Route path="/*" element={<App />} /> {/* Define a route for your App component */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
