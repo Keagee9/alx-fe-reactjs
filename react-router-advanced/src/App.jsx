582b587d803e8f791fb91a69cadd3f376dc796d6
Scott0275 <146986956+Scott0275@users.noreply.github.com>
2025-03-04 10:00:00 +0100

    recipe

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -12,4 +12,37 @@ function App() {
   );
 }
 
+
+
+
+
+import React from 'react';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'; // Import routing components
+import PostsComponent from './components/PostsComponent'; // Import your PostsComponent
+import AnotherComponent from './components/AnotherComponent'; // Import another component (create this if needed)
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Posts</Link>
+            </li>
+            <li>
+              <Link to="/another">Another Page</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<PostsComponent />} />
+          <Route path="/another" element={<AnotherComponent />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
 export default App;
