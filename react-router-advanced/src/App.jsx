87b2d2c7b60a1e56a2e6aae55dd92c104a342151
Scott0275 <146986956+Scott0275@users.noreply.github.com>
2025-03-04 15:16:02 +0100

    recipe

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,6 +1,42 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'; // Import routing components
+import Profile from './components/Profile'; // Import your Profile component
+import BlogPost from './components/BlogPost'; // Import your BlogPost component
+import ProtectedRoute from './components/ProtectedRoute'; // Import your ProtectedRoute component
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/blog/1">Blog Post 1</Link>
+            </li>
+            <li>
+              <Link to="/blog/2">Blog Post 2</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/blog/:postId" element={<BlogPost />} />
+
+          <Route element={<ProtectedRoute />}>
+            <Route path="/profile/*" element={<Profile />} />
+          </Route>
+
+          {/* Add other routes here */}
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
 
 
-<Route element={<ProtectedRoute />}>
-  <Route path="/profile/*" element={<Profile />} />
-  <Route path="/blog/:postId" element={<BlogPost />} />
-</Route>
+
