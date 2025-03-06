fd2ccc92a3f5f0e97ba2704be33d823c87da4392
Scott0275 <146986956+Scott0275@users.noreply.github.com>
2025-03-06 17:04:24 +0100

    todo

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -3,20 +3,22 @@ import { useQuery } from 'react-query';
 import axios from 'axios';
 
 const PostsComponent = () => {
-  const { isLoading, error, data: posts, refetch } = useQuery('posts', async () => {
+  const fetchPosts = async () => {
     const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
     return response.data;
-  });
+  };
+
+  const { isLoading, isError, data: posts, error, refetch } = useQuery('posts', fetchPosts);
 
   if (isLoading) return 'Loading...';
 
-  if (error) return `An error has occurred: ${error.message}`;
+  if (isError) return `An error has occurred: ${error.message}`;
 
   return (
     <div>
       <h1>Posts</h1>
       <ul>
-        {posts.map((post) => (
+        {posts && posts.map((post) => (
           <li key={post.id}>
             <h2>{post.title}</h2>
             <p>{post.body}</p>
