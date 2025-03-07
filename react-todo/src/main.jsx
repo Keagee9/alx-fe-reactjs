3f8b0d227afb63b9ceb5f6b13c72672510e45989
Kingsley Francis Kalu <148270780+Keagee9@users.noreply.github.com>
2025-03-07 10:47:33 +0100

    Delete react-todo directory

--- react-todo/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import TodoList from './components/TodoList'; // Import the TodoList component
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TodoList /> {/* Use the TodoList component */}
-  </React.StrictMode>
-);
