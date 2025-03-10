0f69fcaf12eb1efe1a761349951e43439dc18685
Kingsley Francis Kalu <148270780+Keagee9@users.noreply.github.com>
2025-03-10 19:14:48 +0000

    tailwind

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+import './tailwind.css'
+import './index.html'
+
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      Hello Tailwind!
+    </div>
+  )
+}
+
+export default App
