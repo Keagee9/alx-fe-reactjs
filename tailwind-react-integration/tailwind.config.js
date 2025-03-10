94454d0cc9b7a8091295bef658d2b3dc6a8d233d
Kingsley Francis Kalu <148270780+Keagee9@users.noreply.github.com>
2025-03-10 21:34:15 +0000

    updated

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -9,3 +9,21 @@ module.exports = {
     },
     plugins: [],
   }
+
+  module.exports = {
+    purge: [
+      './src//*.html',
+      './src//*.js',
+      './src//*.jsx',
+      './src//*.ts',
+      './src//*.tsx',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
