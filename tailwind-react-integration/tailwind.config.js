c23877ed5dde633ae3a810d2e831102e32845cb1
Kingsley Francis Kalu <148270780+Keagee9@users.noreply.github.com>
2025-03-10 20:05:44 +0000

    adding new files

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,10 +1,11 @@
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./src//*.{js,jsx,ts,tsx}", // Adjust these paths to match your project structure
+      "./src/**/*.{js,jsx,ts,tsx}",
+      "./public/index.html",
     ],
     theme: {
       extend: {},
     },
-    plugins:,
+    plugins: [],
   }
