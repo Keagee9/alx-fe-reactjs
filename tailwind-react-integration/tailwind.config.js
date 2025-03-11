fcecc6944ec20f09a5aa1750385fbf33457b7c77
Kingsley Francis Kalu <148270780+Keagee9@users.noreply.github.com>
2025-03-11 09:36:36 +0000

    Corrected container padding in UserProfile component

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,11 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+      "./public/index.html",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+  module.exports = {
+    purge: [
+      './src//*.html',
+      './src//*.js',
+      './src//*.jsx',
+      './src//*.ts',
+      './src//*.tsx',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
