2d79e0d8c3bc403b9ca292c130f7f63c4fa4e01f
Kingsley Francis Kalu <148270780+Keagee9@users.noreply.github.com>
2025-03-11 10:06:27 +0000

    Corrected container padding in UserProfile componentgi

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 const UserProfile = () => {
   return (
-    <div className="bg-gray-100 my-20 rounded-lg shadow-lg container mx-auto p-4 sm:p-8 md:p-12 max-w-xs sm:max-w-sm md:max-w-md">
+    <div className="bg-gray-100 my-20 rounded-lg shadow-lg container mx-auto sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-md">
       <div className="flex flex-col items-center">
         <img
           src="profile-picture.jpg"
           alt="Profile Picture"
           className="rounded-full w-24 h-24 sm:w-36 sm:h-36 md:w-48 md:h-48"
         />
-        
         <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4 font-bold">John Doe</h1>
         <p className="text-sm sm:text-base md:text-lg text-gray-600 mt-2">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
