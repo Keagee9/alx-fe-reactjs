372130582e1886a4b1e6b3f2a233fe0dd57296bd
Kingsley Francis Kalu <148270780+Keagee9@users.noreply.github.com>
2025-03-25 08:37:15 +0000

    Initial commit: Setup React project

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">GitHub User Search</h1>
+      {/* Components will go here */}
+    </div>
+  );
+}
+
+export default App;
