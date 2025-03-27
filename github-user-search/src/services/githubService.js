c2120f9ed465d1b5e827a1121a5bb237acd653c8
Kingsley Francis Kalu <148270780+Keagee9@users.noreply.github.com>
2025-03-27 08:04:47 +0000

    modify code

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -8,8 +8,30 @@ export const fetchUserData = async (username) => {
     return response.data;
   } catch (error) {
     if (error.response && error.response.status === 404) {
-      throw new Error('User not found'); // Specific error for 404
+      throw new Error('User not found');
     }
-    throw error; // Re-throw other errors
+    throw error;
+  }
+};
+
+export const searchUsers = async (query) => {
+  try {
+    let url = `${GITHUB_API_BASE_URL}/search/users?q=`;
+    if (query.username) {
+      url += `${query.username}+`;
+    }
+    if (query.location) {
+      url += `location:${query.location}+`;
+    }
+    if (query.minRepos) {
+      url += `repos:>=${query.minRepos}+`;
+    }
+    url = url.slice(0, -1); // Remove trailing '+'
+    url += `&page=${query.page}`;
+
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw error;
   }
 };
